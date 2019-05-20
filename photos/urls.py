09570291550e31a8c0f4ce61c40d7d2793ca5408
Jaccojwang <danolago@gmail.com>
2019-05-20 10:58:32 +0300

    Create urls Create myprofile and edit myprofile urls Create serach url Create other profiles url

--- photos/urls.py
+++ photos/urls.py
@@ -6,6 +6,10 @@ from . import views
 urlpatterns=[
     url('^$', views.home, name = 'home'),
     url(r'^upload$', views.upload, name='upload'),
+    url(r'myprofile/$',views.myprofile,name='profile'),
+    url(r'myprofile/edit$',views.edit,name='edit'),
+    url(r'profile/user/(\d+)/$',views.profile,name='users'),
+    url(r"^search/",views.search,name="search")
 ]
 
 if settings.DEBUG:
