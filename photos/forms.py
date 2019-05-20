e34342ea3733a7d3ca539578ec0733daa9245c2a
Jaccojwang <danolago@gmail.com>
2019-05-20 11:26:39 +0300

    Create required forms

--- photos/forms.py
+++ photos/forms.py
@@ -1,4 +1,4 @@
-from .models import Image
+from .models import Image, Comments, Followers, Profile
 from django import forms
 from django.forms import ModelForm, Textarea, IntegerField
 
@@ -7,3 +7,28 @@ class NewImageForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['']
+
+class EditProfile(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['userId']
+
+class UpdateProfile(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['userId']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model=Comments
+        exclude=['user','images']
+
+class Likes(forms.ModelForm):
+    class Meta:
+        model=Image
+        exclude=['likes','comments','date','user','userId','profile','image','name','caption']
+
+class FollowForm(forms.ModelForm):
+    class Meta:
+        model=Followers
+        exclude=['user','insta','user_id']
