c7166da8dca714164c60b043b4a0af41a9878484
Jaccojwang <danolago@gmail.com>
2019-05-20 08:09:25 +0300

    Create Home view

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import Image
 
-# Create your views here.
+
+def home(request):
+    images = Image.all_images()
+    row = round(len(images)/4)
+    images1 = Image.all_images()[0:row]
+    images2 = Image.all_images()[row:row*2]
+    images3 = Image.all_images()[row*2:row*3]
+    images4 = Image.all_images()[row*3:row*4]
+    return render(request, 'home.html', {"images": images, "images1": images1, "images2": images2, "images3": images3, "images4": images4})
