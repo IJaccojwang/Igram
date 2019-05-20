6723ebf388538dc859d594a8e6682ff0ad082a24
Jaccojwang <danolago@gmail.com>
2019-05-20 08:16:38 +0300

    Finish authentication by adding login backends

--- photos/views.py
+++ photos/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 import datetime as dt
 from .models import Image
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url='/accounts/login/')
 def home(request):
     images = Image.all_images()
     row = round(len(images)/4)
@@ -12,3 +13,4 @@ def home(request):
     images3 = Image.all_images()[row*2:row*3]
     images4 = Image.all_images()[row*3:row*4]
     return render(request, 'home.html', {"images": images, "images1": images1, "images2": images2, "images3": images3, "images4": images4})
+    
